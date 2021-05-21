81b898bdae334729cc1243a6593aaf9ad025e071
Renato Ribeiro <renato.ribeiro@ucode.pt>
2021-05-21 23:31:39 +0100

    Lesson -  195

---
+++ Interfaces/Entities/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Interfaces.Entities
+{
+    public class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
